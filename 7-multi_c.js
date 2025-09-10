598c8d941e9181142aa46f06894463c9cb4951ce
favouriweala <favouriweala@gmail.com>
2025-09-10 13:48:38 +0100

    fixed the negative output

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,7 +2,7 @@ const score = process.argv.slice(2)[0];
 
 const number = parseInt(score, 10);
 
-if (isNaN(number) || number <= 0) {
+if (isNaN(number)) {
     console.log('Missing number of occurrences');
 } else {
     for (let i = 0; i < number; i++) {
