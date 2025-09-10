265efd8d202c98601d863e2f1bb936feca70c24c
favouriweala <favouriweala@gmail.com>
2025-09-10 14:15:54 +0100

    fixed the output errors

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,19 +1,13 @@
-const numArg = process.argv.slice(2)[0];
-
-if (!numArg) {
+const arg = process.argv.slice(2)[0];
+if (arg === undefined) {
     console.log('No argument');
 } 
 else {
-    const n = parseInt(numArg, 10);
+    const n = parseInt(arg, 10);
     function factorial(x) {
-        if (isNaN(x) || x <= 0) {
-            return 1; 
-        }
-        if (x === 1) {
-            return 1; 
-        }
-        return x * factorial(x - 1); 
+        if (isNaN(x) || x <= 0) return 1; 
+        if (x === 1) return 1;
+        return x * factorial(x - 1);
     }
     console.log(factorial(n));
 }
-
