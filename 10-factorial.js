852db7e30eff0f94be5b98a44d39a4b2c9edaa89
favouriweala <favouriweala@gmail.com>
2025-09-10 19:57:44 +0100

    Adds a script that computes and prints a factorial

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,16 +1,14 @@
 const arg = process.argv[2];
 
-if (arg === undefined) {
-    console.log('No argument');
-} else {
-    function factorial(x) {
-        x = parseInt(x, 10);
-        if (isNaN(x) || x <= 0) return 1;
-        if (x === 1) return 1;
-        return x * factorial(x - 1);
-    }
+const n = parseInt(arg, 10);
 
-    console.log(factorial(arg));
+function factorial(x) {
+    if (isNaN(x) || x <= 1) {
+        return 1;
+    }
+    return x * factorial(x - 1);
 }
+
+console.log(factorial(n));
 
 
