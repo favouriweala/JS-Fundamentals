6256409b8ecf3b2c8ee547990945af667c986388
favouriweala <favouriweala@gmail.com>
2025-09-10 14:31:30 +0100

    fixed

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,15 +1,15 @@
-const arg = process.argv[2]; 
+const arg = process.argv[2];
 
-if (arg === undefined) { 
-    console.log('No argument'); 
-} else { 
-    const n = parseInt(arg, 10); 
-    function factorial(x) { 
-        if (isNaN(x) || x <= 0) return 1; 
-        if (x === 1) return 1;             
-        return x * factorial(x - 1);       
-    } 
+if (!arg) {
+    console.log('No argument');
+} else {
+    function factorial(x) {
+        x = parseInt(x, 10);
+        if (isNaN(x) || x <= 0) return 1;
+        if (x === 1) return 1;
+        return x * factorial(x - 1);
+    }
 
-    console.log(`Factorial ${factorial(n)}`);  
+    console.log(factorial(arg));
 }
 
